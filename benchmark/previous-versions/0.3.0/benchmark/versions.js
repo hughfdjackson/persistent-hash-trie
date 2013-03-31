90ca05b7aa84c91b91f8092f4fd702722cc5899f
hugh fd jackson <hughfdjackson@googlemail.com>
2013-03-31 03:02:52 +0100

    Added benchmark for 0.3.1

--- benchmark/previous-versions/0.3.0/benchmark/versions.js
+++
@@ -1,5 +0,0 @@
-module.exports = [
-	{ name: 'current', module: require('..') },
-	{ name: 'v0.2.3',  module: require('./previous-versions/0.2.3/') },
-	{ name: 'v0.2.4',  module: require('./previous-versions/0.2.4/') }
-]
