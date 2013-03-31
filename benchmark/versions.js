8c81c9ca37f021684f787f316c6de87ffda5539e
hugh fd jackson <hughfdjackson@googlemail.com>
2013-03-31 03:05:10 +0100

    Removed previous version from benchmarking versions

--- benchmark/versions.js
+++ benchmark/versions.js
@@ -1,7 +1,6 @@
 module.exports = [
 	{ name: 'shallow-copy reference ', module: require('./reference-versions/shallow-copy.js') },
 	{ name: 'current                ', module: require('..') },
-	{ name: 'v0.2.3                 ', module: require('./previous-versions/0.2.3/') },
 	{ name: 'v0.2.4                 ', module: require('./previous-versions/0.2.4/') },
 	{ name: 'v0.3.0                 ', module: require('./previous-versions/0.3.0/') }
 ]
